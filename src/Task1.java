e32660212479517e31fbd21e7aebf4f6cdcbe33d
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-03-01 14:02:10 -0500

    Task1

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        for(int r = 0; r <= 30; r++)
+        {
+            System.out.print(r + " ");
+        }
+
+
+    }
+}
