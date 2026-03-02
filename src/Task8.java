8b1ce26bc96306c25f427fe7ff72dea4869c0586
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-03-02 13:17:53 -0500

    Task 8

---
+++ src/Task8.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+        Random gen = new Random();
+        int die1 = 0;
+        int die2 = 1;
+        int die3 = 2;
+        int dieSum;
+        int rollNum = 0;
+
+        System.out.println("Roll:      Die1 Die2 Die3 Sum");
+        System.out.println("-----------------------------");
+        while(!(die1 == die2 && die2 == die3))
+        {
+            rollNum ++;
+            die1 = gen.nextInt(6) + 1;
+            die2 = gen.nextInt(6) + 1;
+            die3 = gen.nextInt(6) + 1;
+            dieSum = die1 + die2 + die3;
+
+            System.out.printf("Roll: %-5d %3d %3d %3d %4d\n", rollNum, die1, die2, die3, dieSum);
+        }
+        System.out.println("-----------------------------");
+    }
+}
